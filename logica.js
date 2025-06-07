7e1beeb7633096908c953704fbb6160f1d4b4a12
Igor da Silva Dias <artscolha@gmail.com>
2025-06-07 09:56:30 -0300

    uisff

--- logica.js
+++ logica.js
@@ -3,7 +3,13 @@ function mais(){
     numero += 1
     render()
 }
+function reset(){
+    numero = 0
+    render()
+}
 function render(){
     const p = document.querySelector("button")
     p.innerHTML = numero
+    const zero = document.querySelector("#zero")
+    zero.innerHTML = numero
 }
